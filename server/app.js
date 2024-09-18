83eb34d1ba201d8138c26ab4e86119e327c4fad3
rahul12km <bestrahul123r@gmail.com>
2024-09-18 17:51:45 +0530

    resiter login controller

--- server/app.js
+++ server/app.js
@@ -2,10 +2,17 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const userRouter = require("./routes/user");
 
 
 app.use(cors());
 app.use(express.json());
+const ConnectDB = require("./config/db");
+
+ConnectDB();
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user",userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
